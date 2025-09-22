d113e2a328cfd8dcf906697b7f203bdeecb04137
vamsigajula14 <gajulavamsi90@gmail.com>
2025-09-22 18:10:23 +0530

    Database Schema done

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,45 @@
-const mangoose = require('mangoose');
+const mongoose = require("mongoose");
+const { string } = require("zod");
+require('dotenv').config();
+const url = process.env.mongoUrl;
+try{
 
-mangoose.connect('mongodb+srv://vamsi:<db_password>@cluster0.ezkypim.mongodb.net/')
+    mongoose.connect(url);
 
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true,
+        minLength : 3,
+        maxLength : 30
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 6
+    },
+    firstname : {
+        type : String,
+        required : true,
+        trim : true,
+        maxLength : 50
+    },
+    lastname : {
+        type : String,
+        required : true,
+        trim : true,
+        maxLength : 50
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+console.log("Schema created");
+module.export = {
+    User
+}    
+}catch(err){
+    console.log(err.message);
+}
